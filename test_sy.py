5a3c72500954ad50f043a689609224647f967a23
siyiing <ongsiying00@gmail.com>
2022-11-13 05:36:41 +0800

    Remove extra files

--- test_sy.py
+++
@@ -1,9 +0,0 @@
-import preprocessing
-
-# test code to verify correctness
-def test_sy():
-    param = ['Bitmap Scan', 'Hash Join', 'Nested Loop Join', 'Hashed Aggregation', 'Materialization', 'Explicit Sort']
-    preprocessing.permutation()
-
-
-test_sy()
