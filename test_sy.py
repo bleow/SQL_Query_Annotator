7d878647c3034e11d469e7fcce12d348ec253122
bleow <bryankhleow@gmail.com>
2022-11-13 14:00:52 +0800

    Revert "Remove extra files"
    This reverts commit 5a3c72500954ad50f043a689609224647f967a23.

---
+++ test_sy.py
@@ -0,0 +1,9 @@
+import preprocessing
+
+# test code to verify correctness
+def test_sy():
+    param = ['Bitmap Scan', 'Hash Join', 'Nested Loop Join', 'Hashed Aggregation', 'Materialization', 'Explicit Sort']
+    preprocessing.permutation()
+
+
+test_sy()
