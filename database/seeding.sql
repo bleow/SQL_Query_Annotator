b9b2160b60ed5765a69fa596beddb603fbc43a0d
bleow <bryankhleow@gmail.com>
2022-11-13 17:05:59 +0800

    chore: remove files for submission

--- database/seeding.sql
+++
@@ -1,4 +0,0 @@
--- https://stackoverflow.com/questions/22458464/why-do-i-get-the-extra-data-after-last-expected-column-when-importing-csv-int
-COPY lineitem
-FROM 'C:\.PROGRAMMING\SQL_Query Visualiser\database\lineitem.csv' 
-CSV
